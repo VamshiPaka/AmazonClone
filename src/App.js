c7cf0e4d6551cfa335ab9fb8d84acfb00b2d61c3
VamshiPaka <vamshi.paka@gmail.com>
2021-03-21 20:35:00 +0530

    props added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Greet(props) {
+  return <div>Hello {props.name}</div>;
+}
+
 function App() {
-  return <div className="App">My Component</div>;
+  return (
+    <div className="App">
+      <Greet name="Vamshi" />
+      <Greet name="Karthik" />
+      <Greet name="Sanju" />
+      <Greet name="Keerthi" />
+    </div>
+  );
 }
 
 export default App;
