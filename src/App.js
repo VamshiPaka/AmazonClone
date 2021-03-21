dc47e30d2665244bdcf8be0be069df5af8550e80
VamshiPaka <vamshi.paka@gmail.com>
2021-03-21 20:42:25 +0530

    counter added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function Greet(props) {
   return <div>Hello {props.name}</div>;
+}
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  function increase() {
+    setCount(count + 1);
+  }
+  return (
+    <div>
+      My Count is {count}
+      <button onClick={increase}>+</button>
+    </div>
+  );
 }
 
 function App() {
@@ -12,6 +26,7 @@ function App() {
       <Greet name="Karthik" />
       <Greet name="Sanju" />
       <Greet name="Keerthi" />
+      <Counter />
     </div>
   );
 }
